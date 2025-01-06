205d21fe40179b7d32078c3c7d0be36ba3073e89
Nikhil Yadav <facts.foundr@gmail.com>
2025-01-06 21:34:15 +0530

    Initialized

---
+++ components/ui/Button.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { ReactElement } from "react";
+
+interface ButtonInterface{
+  title : string;
+  size : "sm" | "md" |"lg";
+  startIcon?: ReactElement,
+  endIcon?: ReactElement,
+  variant : 'primary'|'secondary'|'submit'|'disable',
+  onClick?: () => void;
+  loading?:boolean
+
+}
+
+const sizeStyles = {
+  "sm" :"rounded-sm px-2 py-2 text-sm",
+  "md" :" rounded-md px-4 py-2 text-md",
+  "lg" :"rounded-lg px-8 py-4 text-xl",
+}
+
+const ButtonVariants = {
+  "primary" : ' hover:bg-opacity-90 bg-blue-600 text-white',
+  "secondary" : " hover:bg-opacity-80 bg-blue-200 text-blue-600 ",
+  "disable" : " hover:bg-opacity-80 bg-gray-200  text-blue-600 w-full",
+  "submit" :  'w-full hover:bg-opacity-90 bg-blue-600 text-white',
+}
+
+export  function Button(props : ButtonInterface){ 
+  return (
+    <button  onClick={props.onClick} className={`flex  items-center justify-center ${ButtonVariants[props.variant]} ${props.loading && 'bg-opacity-50' }  ${sizeStyles[props.size]}`}>
+      {props.startIcon}
+      <span  className="pl-2 pr-2">
+          {props.title}
+      </span>
+      {props.endIcon}
+    </button>
+  )
+}
